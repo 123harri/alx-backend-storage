de53a2994a77703489ac5fe02e61b5ead6369931
123harri <harrietwanjiku90@gmail.com>
2024-07-11 09:47:50 +0000

    a

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,6 @@
--- List bands with Glam rock as their main style, ranked by longevity in years until 2022
-
-SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
-FROM (
-    SELECT band_name, formed,
-           CAST(SPLIT_PART(lifespan, '-', 1) AS UNSIGNED) AS split
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-) AS glam_bands
-ORDER BY lifespan DESC;
+    ORDER BY lifespan DESC;
