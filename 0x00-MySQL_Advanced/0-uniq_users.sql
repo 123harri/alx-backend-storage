26cc6c87a760ce086e2c5248abb69a40b567ef12
123harri <harrietwanjiku90@gmail.com>
2024-07-11 08:36:17 +0000

    a

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- This script creates a 'users' table with specified constraints if it does not exist
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
