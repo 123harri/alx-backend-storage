6fb8a0f8b87c8d93d6ceb32a4de683a7032996c9
123harri <harrietwanjiku90@gmail.com>
2024-07-17 03:44:35 -0700

    a

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,27 @@
 #!/usr/bin/env python3
-"""
-A module with tools for request caching and tracking.
-"""
+'''A module with tools for request caching and tracking using Redis.
+'''
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
+
 redis_store = redis.Redis()
+'''The module-level Redis instance for
+caching and tracking requests.
+'''
 
 
 def data_cacher(method: Callable) -> Callable:
-    """
-    A decorator that caches the output of fetched data
-    and tracks the number of requests.
-
-    Args:
-        method (Callable): The function to be decorated.
-
-    Returns:
-        Callable: The wrapped function with
-        caching and tracking functionality.
-    """
+    '''Decorator that caches the output of
+    fetched data and tracks request counts.
+    '''
     @wraps(method)
     def invoker(url: str) -> str:
-        """
-        Wrapper function for caching the output and tracking requests.
-
-        Args:
-            url (str): The URL to fetch data from.
-
-        Returns:
-            str: The content of the URL.
-        """
+        '''Wrapper function for caching fetched
+        data and tracking requests.
+        '''
         redis_store.incr(f'count:{url}')
 
         result = redis_store.get(f'result:{url}')
@@ -40,6 +29,7 @@ def data_cacher(method: Callable) -> Callable:
             return result.decode('utf-8')
 
         result = method(url)
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
@@ -47,14 +37,7 @@ def data_cacher(method: Callable) -> Callable:
 
 @data_cacher
 def get_page(url: str) -> str:
-    """
-    Fetches the content of a URL, caches the
-    response, and tracks the request.
-
-    Args:
-        url (str): The URL to fetch data from.
-
-    Returns:
-        str: The content of the URL.
-    """
+    '''Fetches the content of a URL, caches the
+    response, and tracks the request count.
+    '''
     return requests.get(url).text
